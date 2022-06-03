a79518a151d225f45f870536075dc6f0b4f0c64e
Luis Fernando Cuevas Cuauhtle <luisfernandocuevas@outlook.com>
2022-06-02 19:37:30 -0500

    Deleted previous work on Collection View, it was getting too messy and too complicated.

---
+++ KnowVegan/3HomePage/TableViewCell/CollectionViewCell/FruitsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsCollectionViewCell.swift
+//  KnowVegan
+//
+//  Created by Luis Fernando Cuevas Cuauhtle on 2/6/22.
+//
+
+import UIKit
+
+class FruitsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
